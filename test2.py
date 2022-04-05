51e067bec64264cdb259bf10b08dc8f665c8f71e
Garth Wales <git@garth.wales>
2022-04-06 11:35:19 +1200

    fix error

--- test2.py
+++ test2.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 def main():
     input = torch.randn(32, 1, 32, 32)
     print(f'input {input.shape}')
-    for i in input:
-        print(input[i,...])
+    # for i in input:
+    #     print(input[i,...])
 
     upsample = nn.ConvTranspose2d(1, 1, kernel_size=5, stride=1, padding=0) # in_channels, out_channels = 1 for black and white
     upsample2 = nn.ConvTranspose2d(1, 1, kernel_size=3, stride=1, padding=0)
