a0cc22983e7b712fd6c26c766893029c25aa0322
Garth Wales <git@garth.wales>
2022-05-04 00:33:36 +1200

    add weights network

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 # local imports
 from nc import NormalizedCuts
 from node import DeclarativeLayer
+from data import get_weights_vars
 
 class Net(nn.Module):
     def __init__(self, args):
@@ -18,6 +19,35 @@ class Net(nn.Module):
         x = self.decl(x) # check the size of this output...
         x = self.postNC(x)
         return x
+
+class WeightsNet(nn.Module):
+    def __init__(self, args):
+        super(WeightsNet, self).__init__()
+        self.r, self.min = get_weights_vars(args)
+        self.block1 = self.conv_block(c_in=args.net_size[0], c_out=args.net_size[1], kernel_size=3, stride=1, padding=1)
+        self.block2 = self.conv_block(c_in=args.net_size[1], c_out=args.net_size[2], kernel_size=3, stride=1, padding=1)
+        self.block3 = self.conv_block(c_in=args.net_size[2], c_out=args.net_size[3], kernel_size=3, stride=1, padding=1)
+        self.lastcnn = nn.Conv2d(in_channels=args.net_size[3], out_channels=args.net_size[4], kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.relu(self.lastcnn(x))
+
+        # combine the 32x32 image filters into the correct output size (full matrix or not...)
+        # NOTE: this may end up having to align with net_size[4] and be caluclated...
+        if self.min: x = x.view(x.size(0), 1, self.r, 1024) 
+        else: x = x.view(x.size(0), 1, 1024, 1024) # full matrix (with majority zeros)
+
+        return x
+    def conv_block(self, c_in, c_out, **kwargs):
+        seq_block = nn.Sequential(
+            nn.Conv2d(in_channels=c_in, out_channels=c_out, **kwargs),
+            nn.BatchNorm2d(num_features=c_out),
+            nn.ReLU()
+            )
+        return seq_block
 
 class PreNC(nn.Module):
     def __init__(self, args):
