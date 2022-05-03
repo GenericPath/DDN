e2c2faa054939c741796fef75b763b06123fd6e9
Garth Wales <git@garth.wales>
2022-05-03 23:05:36 +1200

    average losses, average accuracies

--- model_loops.py
+++ model_loops.py
@@ -6,8 +6,7 @@ from tqdm import tqdm
 def test(val_loader, model, criterion):
     model.eval()
     with torch.no_grad():
-        avg_acc = 0
-        avg_loss = 0
+        avg_acc, avg_loss = 0,0
         for input_batch, target_batch in tqdm(val_loader, desc=val_accuracy):
             input_batch, target_batch = input_batch, target_batch
 
@@ -19,7 +18,7 @@ def test(val_loader, model, criterion):
             
             avg_acc += val_accuracy
             avg_loss += val_loss.item()
-            
+
         avg_acc /= len(val_loader)
         avg_loss /= len(val_loader)
     return avg_acc, avg_loss
@@ -27,26 +26,28 @@ def test(val_loader, model, criterion):
 def validate(val_loader, model, device, criterion, scheduler):
     model.eval()
     with torch.no_grad():
+        val_accuracy, val_loss = 0,0
         for input_batch, target_batch in val_loader:
             input_batch, target_batch = input_batch.to(device), target_batch.to(device)
 
             output = model(input_batch)
-            val_loss = criterion(output, target_batch)
+            val_loss += criterion(output, target_batch)
             scheduler.step(val_loss) # Reduce LR on plateu
 
             output = (output > 0.5).float()         
-            val_accuracy = output.eq(target_batch).float().mean()
-
+            val_accuracy += output.eq(target_batch).float().mean()
+            
+        val_accuracy /= len(val_loader)
+        val_loss /= len(val_loader)
     return val_accuracy, val_loss
 
 def train(train_loader, model, device, criterion, optimizer):
     model.train()
-    per_epoch_loss = 0
+    train_accuracy, train_loss = 0,0
     for input_batch, target_batch in tqdm(train_loader):
         input_batch, target_batch = input_batch.to(device), target_batch.to(device)
         output = model(input_batch)
         loss = criterion(output, target_batch)
-        per_epoch_loss += loss
         # Compute gradient and do optimizer step
         optimizer.zero_grad()
         if not torch.isnan(loss).any():
@@ -54,6 +55,9 @@ def train(train_loader, model, device, criterion, optimizer):
         optimizer.step()
 
         output = (output > 0.5).float()
-        train_accuracy = output.eq(target_batch).float().mean()
-        
-    return train_accuracy, loss.item()
+        train_accuracy += output.eq(target_batch).float().mean()
+        train_loss += loss.item()
+
+    train_accuracy /= len(train_loader)
+    train_loss/= len(train_loader)
+    return train_accuracy, train_loss
