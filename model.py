6d78ee75d4093e840df9e6361bad046f5b40f2d4
Garth Wales <git@garth.wales>
2022-05-29 17:52:09 +1200

    add de_minW to model, rearannge code a bit + add comments

--- model.py
+++ model.py
@@ -2,10 +2,13 @@ import torch
 import torch.nn as nn
 
 # local imports
-from nc import NormalizedCuts
+from nc import NormalizedCuts, de_minW
 from node import DeclarativeLayer
 
 class Net(nn.Module):
+    """
+    WeightsNet -> NC -> PostNC
+    """
     def __init__(self, args):
         super(Net, self).__init__()
         self.weightsNet = WeightsNet(args)
@@ -20,6 +23,10 @@ class Net(nn.Module):
         return x
 
 class WeightsNet(nn.Module):
+    """
+    Just learns the weights (to feed into NC)
+    if args.min then learns only the diagonals (as the rest is sparse)
+    """
     def __init__(self, args):
         super(WeightsNet, self).__init__()
         self.r = args.radius
@@ -29,16 +36,19 @@ class WeightsNet(nn.Module):
         self.block3 = self.conv_block(c_in=args.net_size[2], c_out=args.net_size[3], kernel_size=3, stride=1, padding=1)
         if self.min: self.lastcnn = nn.Conv2d(in_channels=args.net_size[3], out_channels=self.r+1, kernel_size=3, stride=1, padding=1)
         else: self.lastcnn = nn.Conv2d(in_channels=args.net_size[3], out_channels=args.net_size[4], kernel_size=3, stride=1, padding=1)
-        self.relu = nn.ReLU()
+        self.restrict = nn.Sigmoid() # was previously a ReLU
     def forward(self, x):
         x = self.block1(x)
         x = self.block2(x)
         x = self.block3(x)
-        x = self.relu(self.lastcnn(x))
+        x = self.restrict(self.lastcnn(x))
 
         # combine the 32x32 image filters into the correct output size (full matrix or not...)
-        if self.min: x = x.view(x.size(0), 1, self.r+1, 1024) # r+1 as radius + itself
-        else: x = x.view(x.size(0), 1, 1024, 1024) # full matrix (with majority zeros)
+        if self.min: 
+            x = x.view(x.size(0), 1, self.r+1, 1024) # r+1 as radius + itself
+            x = de_minW(x)
+        else: 
+            x = x.view(x.size(0), 1, 1024, 1024) # full matrix (with majority zeros)
 
         return x
     def conv_block(self, c_in, c_out, **kwargs):
