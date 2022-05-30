cab63bff9b35bc9e6b73abd397d23344c8f4462a
Garth Wales <git@garth.wales>
2022-05-31 02:15:02 +1200

    switch to net_argparser function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import torch.utils.tensorboard as tb
 import torch
 
-import argparse, random, os, shutil
+import random, os, shutil
 
 import torch.nn as nn
 import torch.optim as optim
@@ -12,46 +12,14 @@ import torch.backends.cudnn as cudnn
 from data import get_dataset
 from model_loops import test, train, validate
 from model import Net, WeightsNet
+from net_argparser import net_argparser
 
 # Maybe add this later
 # from torchsummary import summary
 
-parser = argparse.ArgumentParser(description='Train the UNet on images and binary target masks')
-parser.add_argument('--name', '-n', type=str, default='test', help='Tensorboard run name')
-parser.add_argument('--epochs', '-e', metavar='E', type=int, default=20, help='Number of epochs')
-parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
-parser.add_argument('--learning-rate', '-lr', metavar='LR', type=float, default=1e-4,
-                    help='Learning rate', dest='lr')
-parser.add_argument('--momentum', '-m', metavar='M', type=float, default=0.9, help='momentum')
-parser.add_argument('--validation', '-v', dest='val', type=float, default=0.1,
-                    help='Percent of the data that is used as validation (0-1)')
-parser.add_argument('--seed', '-s', metavar='S', type=int, default=None, help='Seed to get consistent outcomes')
-parser.add_argument('--total-images', '-ti', metavar='N', type=int, default=300, dest='total_images', help='total number of images in dataset')
-parser.add_argument('--net-size', '-ns', metavar='[...]', nargs='+', type=int, default=[1,128,256,512,1024], dest='net_size', help='number of filters for the 3 layers')
-parser.add_argument('--gpu-id', '-gpu', type=str, default='1', dest='gpu', help='which id gpu to utilise (if present)')
-
-# currently no options to use
-parser.add_argument('--optim', '-o', metavar='OPT', type=str, default='sgd', dest='optim', help='optimiser to use')
-parser.add_argument('--shuffle', type=bool, default=True, help='shuffle batches')
-parser.add_argument('--dataset', type=str, default='simple01', help='dataset to use: weights (pre), simple01 (pre, NC, post')
-
-# newer
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N', help='manual epoch number (useful on restarts)')
-parser.add_argument('--test', action='store_true', help='Whether to test/evaluate or train')
-parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
-parser.add_argument('--production', default=False, type=bool, help='Production mode: If true run in a separate folder on a copy of the python scripts')
-parser.add_argument('--network', default=0, type=int, help='network to use: 0=Net, 1=WeightsNet')
-
-# newest
-parser.add_argument('--minify', default=False, type=bool, help='minify the weights mode (for the PreNC portion)')
-parser.add_argument('--radius', '-r', default=1, type=int, help='radius value for expected weights (only relevant for minified version)')
-
-# TODO : add option to switch between eqconst
-parser.add_argument('--eqconst', default=True, type=bool, help='equality constrained or non equality constrained')
-
 def main():
     # Parse commandline arguments
-    args = parser.parse_args()
+    args = net_argparser()
 
     # Pre-model setup
     if args.seed is not None:
@@ -80,7 +48,7 @@ def main():
         optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
     else:
         optimizer = optim.Adam(model.parameters(), lr=args.lr) # TODO : add weight decay and betas as options
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5) # TODO : adjust patience
 
     # Load the weights of a saved network (if provided)
     if args.resume:
@@ -110,8 +78,8 @@ def main():
     best_error = float('inf')
     for epoch in range(args.start_epoch, args.epochs):
 
-        t_acc, t_loss = train(train_loader, model, device, criterion, optimizer)
-        v_acc, v_loss = validate(val_loader, model, device, criterion, scheduler)
+        t_acc, t_loss = train(train_loader, model, device, criterion, optimizer) # TODO : check if this scheme makes sense (with opt and scheduler...)
+        v_acc, v_loss = validate(val_loader, model, device, criterion, scheduler) # scheduler will change LR on val plateau, optim will
 
         # Currently best is based on acc, could be changed for loss
         is_best = v_acc > best_acc
