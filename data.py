90efbc0ec9aeda776ef365338eb1336282d418b5
Garth Wales <git@garth.wales>
2022-05-31 02:14:12 +1200

    move dataset to single folder simple01, which contains input,output,weights. expands automatically with one folder

--- data.py
+++ data.py
@@ -8,47 +8,21 @@ import cv2
 from torchvision import transforms
 from torch.utils.data import random_split
 
+# local imports
 from nc import de_minW, manual_weight
 
-import argparse
-
-# plot_multiple_images
+# imports for plot_multiple_images
 import matplotlib.pyplot as plt
 import torchvision.transforms.functional as F
 
-def plot_multiple_images(batch_no, images, labels=None, minified=False, figsize=[32,32]):
-    """
-    Images [input_batch, output_batch, ...]
-    """
-    # TODO : use https://pytorch.org/vision/main/auto_examples/plot_visualization_utils.html instead?
-
-    # settings
-    N = min(map(len, images)) # length of the shortest array
-    nrows, ncols = N, len(images)  # array of sub-plots
-
-    # create figure (fig), and array of axes (ax)
-    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize, squeeze=False)
-    # can access individual plot with ax[row][col]
-
-    # plot image on each sub-plot
-    for i, row_ax in enumerate(ax): # could flatten if not explicitly doing in pairs (ax.flat)
-        for j in range(ncols):
-            img = images[j][i].cpu()
-            if j == 1 and minified:
-                img = de_minW(img[None,None,:])[0][0] # exapnd with dummy batches and channels  
-            img = F.to_pil_image(img)
-            row_ax[j].imshow(np.asarray(img))
-            if labels is not None:
-                row_ax[j].set_title(str(labels[i]))
-
-    plt.tight_layout()
-    plt.savefig('experiments/batch-'+str(batch_no)+'.png')
-    plt.close()
-
 def get_dataset(args):
-    train_dataset = SimpleDatasets(args, transform=transforms.ToTensor()) #path/dataset is a pickle containing (image paths, targets)
-
+    """
+    Creates the train_loader, val_loader
+    calls SimpleDatasets() which creates the data (if needed) using data()
+    """
+    train_dataset = SimpleDatasets(args, transform=transforms.ToTensor())
     print(f'Total dataset size {len(train_dataset)}')
+
     # Training and Validation dataset
     n_val = int(len(train_dataset) * args.val)
     n_train = len(train_dataset) - n_val
@@ -61,63 +35,109 @@ def get_dataset(args):
 
     return train_loader, val_loader
 
+
+def plot_multiple_images(batch_no, images, dir='experiments/',labels=None, figsize=[32,32]):
+    """
+    Images [input_batch, output_batch, weights]
+    provide None for elements not present
+    """
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        print(dir + ' has been made')
+    
+    # TODO : use https://pytorch.org/vision/main/auto_examples/plot_visualization_utils.html instead?
+    # settings
+    N = min(map(len, images)) # length of the shortest array
+    nrows, ncols = N, len(images)  # array of sub-plots
+
+    # create figure (fig), and array of axes (ax)
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize, squeeze=False)
+    # can access individual plot with ax[row][col]
+
+    # plot image on each sub-plot
+    for i, row_ax in enumerate(ax): # could flatten if not explicitly doing in pairs (ax.flat)
+        for j in range(ncols):
+            img = images[j][i]
+            if j == 2:
+                # de_minW checks if it is minified weights (r,N) or already something expanded (N,N)
+                img = de_minW(img[None,:])[0] # expand with dummy batches (may need to reduce to [0][0] for single channel plots?)
+
+            img = F.to_pil_image(img)
+            row_ax[j].imshow(np.asarray(img), cmap='gray')
+
+            # useful labels include the calculated accuracy or losses...
+            if labels is not None:
+                row_ax[j].set_title(str(labels[i]))
+
+    plt.tight_layout()
+    plt.savefig(dir+'batch-'+str(batch_no)+'.png')
+    plt.close()
+
+def load_dataset(path, number):
+    """
+    opens the path/dataset file and returns
+    (inputs (images), outputs (segmentations), intermediate (weights))
+    """
+    with open (path+'dataset', 'rb') as fp:
+        output = pickle.load(fp)
+        inputs = output[0][:number] # images
+        outputs = output[1][:number] # segmentations
+        intermediate = output[2][:number] # weights
+    return inputs, outputs, intermediate
+
 def data(path, args, img_size=(32,32)):
     """ Generate a simple dataset (if it doesn't already exist) 
     path - example 'data/simple01/'
     total_images - total number of images to create for the dataset
     image size - (w,h)
     """
-
     if not os.path.exists(path):
         os.makedirs(path)
         print(path + ' has been made')
         
-    if not os.path.isfile(path+'dataset'):
-        images = []
-        answers = []
-        for i in range(args.total_images):
-            # L gives 8-bit pixels (0-255 range of white to black)
-            w,h = (random.randint(img_size[0]//3, img_size[0]), random.randint(img_size[0]//3, img_size[0]))
-            x,y = (random.randint(img_size[0]//3, img_size[0]), random.randint(img_size[0]//3, img_size[0]))
+    images, answers, weights = [],[],[]
+    start = 0
+    if os.path.isfile(path+'dataset'):
+        # already been made once, will append images to increase count
+        images, answers, weights = load_dataset(path, args.total_images)
+        start = len(images)
+    print(f'loaded {start} existing images')
 
-            xy = [(x-w//2,y-h//2), (x+w//2,y+h//2)]
-            answer = np.zeros(img_size)
-            answer[xy[0][0]:xy[1][0], xy[0][1]:xy[1][1]] = 1
+    for i in range(start, args.total_images):
+        # TODO: When creating dataset, enforce constraint of 50% white, 50% black to ensure nothing funny is happening
 
-            # L gives 8-bit pixels (0-255 range of white to black)
-            out = Image.fromarray(np.uint8(answer * 255), 'L')
+        # L gives 8-bit pixels (0-255 range of white to black)
+        w,h = (random.randint(img_size[0]//3, img_size[0]), random.randint(img_size[0]//3, img_size[0]))
+        x,y = (random.randint(img_size[0]//3, img_size[0]), random.randint(img_size[0]//3, img_size[0]))
 
-            name = path+"img"+str(i)+".png"
-            out.save(name, "PNG")
-            images.append(name)
-            
-            if 'weights' == args.dataset: # append weights matrix as answer TODO : make it create a separate weights dataset when making the main one... so they are the same images but for different stuff?
-                answers.append(manual_weight(name, r=args.radius, minVer=args.minify).squeeze()) 
-            else: 
-                answers.append(answer) # otherwise append output as answer (for simple01, etc)
+        xy = [(x-w//2,y-h//2), (x+w//2,y+h//2)]
+        answer = np.zeros(img_size)
+        answer[xy[0][0]:xy[1][0], xy[0][1]:xy[1][1]] = 1
 
-        # write the answers to a txt file to visually inspect (while initially setting everything up)
-        ans_out = open(path+'answers'+'.txt', 'w')
-        if 'weights' == args.dataset and args.radius == 1:
-            for row in answers:
-                for item in row:
-                    ans_out.write(str(item.item()) + ' ')
-                ans_out.write('\n')
-            ans_out.write('\n---\n')
-        else:
-            for answer in answers: # [[b,c,row,item],...]
-                for rows in answer:
-                    for item in rows:
-                        ans_out.write(str(item.item()) + ' ')
-                    ans_out.write('\n')
-                ans_out.write('\n---\n')
-        ans_out.close()
+        # L gives 8-bit pixels (0-255 range of white to black)
+        out = Image.fromarray(np.uint8(answer * 255), 'L')
 
-        # save the input, output pairs to a file
-        output = [images, answers]
+        name = path+"img"+str(i)+".png"
+        out.save(name, "PNG")
+        images.append(name)
+        
+        weights.append(manual_weight(name, r=args.radius, minVer=args.minify).squeeze(0)) 
+        # squeeze(0) to remove batch dimension (but maintain channels or radius which could be 1)
+        answers.append(answer) 
+
+    if start != args.total_images: # if there were new images to create, then save the new pickle
+        # write the new contents to disk
+        output = [images, answers, weights]
         with open(path+'dataset', 'wb') as fp:
             pickle.dump(output, fp)
-        print("made the dataset file")
+        print(f"made the dataset file, {i - (start-1)} new images")
+
+        # plot one example of the image, segmentation and weights
+        print('create batch-num.pngs')
+        train_dataset = SimpleDatasets(args, transform=transforms.ToTensor())
+        for i in range(max(args.total_images-5,0),args.total_images):
+            row = [train_dataset.get_image(i), train_dataset.get_segmentation(i), train_dataset.get_weights(i)]
+            plot_multiple_images(i, row, dir=path)
 
 class SimpleDatasets(Dataset):
     """ Simple white background, black rectangle dataset """
@@ -126,73 +146,56 @@ class SimpleDatasets(Dataset):
         """
         file (string): Path to the pickle that contains [img paths, output arrays]
         """
-        self.args = args
-        # append = '../../' if args.production else ''
-        append = ''
-        path = append + 'data/' + args.dataset + '/' + str(args.total_images) # location to store dataset
-        if args.minify:
-            path += 'min' + str(args.radius) + '/'
-        else:
-            path += '/'
-        data(path, args) # make the dataset
+        self.network = args.network
+        self.total_images = args.total_images
+        path = 'data/' + args.dataset + '/' # location to store dataset
 
-        with open (path+'dataset', 'rb') as fp:
-            output = pickle.load(fp)
-            self.images = output[0] # images
-            self.segmentations = output[1] # segmentation
+        data(path, args) # make the dataset
+        self.images, self.segmentations, self.weights = load_dataset(path, self.total_images)
+        
         self.transform = transform
+        print(f'loaded {path}dataset')
             
     def __len__(self):
         return len(self.images)
     
     def __getitem__(self, index):
-        img = cv2.imread(self.images[index], 0)
-        # or switch to PIL.Image.open() and then img.load()?
-        y_label = self.segmentations[index]
-        
+        # 1. load image
+        img = cv2.imread(self.images[index], 0) # or switch to PIL.Image.open() and then img.load()?
         if self.transform is not None:
             img = self.transform(img)
-            if 'weights' not in self.args.dataset:
+        
+        # 2. load target (based on network)
+        if self.network == 1: # weights
+            y_label = self.weights[index]
+        else: # simple01
+            y_label = self.segmentations[index]
+            if self.transform is not None: 
                 y_label = self.transform(y_label)
-            
         return (img, y_label)
+    
+    # TODO: actually use these helper functions
+    def get_image(self, index):
+        image = cv2.imread(self.images[index])
+        return self.transform(image) if self.transform is not None else image
+
+    def get_segmentation(self, index):
+        segmentation = self.segmentations[index]
+        return self.transform(segmentation) if self.transform is not None else segmentation
+
+    def get_weights(self, index):
+        return de_minW(self.weights[index][None,:])
 
 if __name__ == '__main__':
+    from net_argparser import net_argparser
 
-    parser = argparse.ArgumentParser(description='debugging arguments')
-    # main args
-    parser.add_argument('--dataset', type=str, default='weights', help='dataset to use: weights, simple01')
-    parser.add_argument('--total-images', '-ti', metavar='N', type=int, default=10, dest='total_images', help='total number of images in dataset')
-    parser.add_argument('--production', default=False, type=bool, help='Production mode: If true run in a separate folder on a copy of the python scripts')
-
-    # minify arguments
-    parser.add_argument('--minify', default=True, type=bool, help='minify the weights mode (for the PreNC portion)')
-    parser.add_argument('--radius', '-r', default=1, type=int, help='radius value for expected weights (only relevant for minified version)')
-
-    # get_dataset args
-    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
-    parser.add_argument('--validation', '-v', dest='val', type=float, default=0.1,
-                    help='Percent of the data that is used as validation (0-1)')
-    parser.add_argument('--shuffle', type=bool, default=True, help='shuffle batches')
-
-    args = parser.parse_args()
-    args.production = False
-
-    append = ''
-    path = append + 'data/' + args.dataset + '/' + str(args.total_images) # location to store dataset
-    if args.minify:
-        path += 'min' + str(args.radius) + '/'
-    else:
-        path += '/'
-
-    data(path, args, img_size=(32,32))
+    args = net_argparser()
+    args.network = 1
+    
     train_loader, val_loader = get_dataset(args)
-    for i, (input, output) in enumerate(train_loader):
-        plot_multiple_images(i, [input, output], minified=(args.dataset == 'weights'))
-        print(i)
-        if i > 10:
-            exit()
 
-    # test the dataset (and test plot_multiple_images)
-    # plot_multiple_images
-
+    print('load again to create experiments/')
+    train_dataset = SimpleDatasets(args, transform=transforms.ToTensor())
+    for i in range(5):
+        row = [train_dataset.get_image(i), train_dataset.get_segmentation(i), train_dataset.get_weights(i)]
+        plot_multiple_images(i, row, dir='experiments/')
