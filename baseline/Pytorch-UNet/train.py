af14246e01e36bb03b14c37295abc9e0d227fe8c
Garth Wales <git@garth.wales>
2022-08-09 23:08:57 +1200

    switch to be compatible with wandb sweeps, testing now if it works

--- baseline/Pytorch-UNet/train.py
+++ baseline/Pytorch-UNet/train.py
@@ -18,63 +18,57 @@ from unet import UNet
 
 
 
-dir_img = Path('../../data/tc/img/')
-dir_mask = Path('../../data/tc/maskC/')
 dir_checkpoint = Path('../../data/tc/')
+base_path = '../../data/'
 
 
-def train_net(net,
-              device,
-              epochs: int = 5,
-              batch_size: int = 10,
-              learning_rate: float = 1e-5,
-              val_percent: float = 0.1,
-              save_checkpoint: bool = True,
-              img_scale: float = 1,
-              amp: bool = False):
+def train_net(net, args, experiment, save_checkpoint = True):
+
     # 1. Create dataset
-    dataset = BasicDataset(dir_img, dir_mask, scale=img_scale, transform = transforms.ToTensor())
+    dataset = BasicDataset(base_path+args.dir_img, base_path+args.dir_mask, scale=args.img_scale, transform = transforms.ToTensor())
+    # TODO: copy from https://github.com/borisdayma/lightning-kitti/blob/master/train.py
 
     # 2. Split into train / validation partitions
+    val_percent = args.val / 100
     n_val = int(len(dataset) * val_percent)
     n_train = len(dataset) - n_val
     train_set, val_set = random_split(dataset, [n_train, n_val], generator=torch.Generator().manual_seed(0))
 
     # 3. Create data loaders
-    loader_args = dict(batch_size=batch_size, num_workers=4, pin_memory=True)
+    loader_args = dict(batch_size=args.batch_size, num_workers=4, pin_memory=True)
     train_loader = DataLoader(train_set, shuffle=True, drop_last=True,**loader_args)
     val_loader = DataLoader(val_set, shuffle=False, drop_last=True, **loader_args)
 
     # (Initialize logging)
-    experiment = wandb.init(project='U-Net', resume='allow', anonymous='must')
-    experiment.config.update(dict(epochs=epochs, batch_size=batch_size, learning_rate=learning_rate,
-                                  val_percent=val_percent, save_checkpoint=save_checkpoint, img_scale=img_scale,
-                                  amp=amp))
+    # experiment = wandb.init(project='U-Net', resume='allow', anonymous='must')
+    # experiment.config.update(dict(epochs=args.epochs, batch_size=args.batch_size, learning_rate=args.lr,
+    #                               val_percent=args.val_percent, save_checkpoint=save_checkpoint, img_scale=args.img_scale,
+    #                               amp=amp))
 
     logging.info(f'''Starting training:
-        Epochs:          {epochs}
-        Batch size:      {batch_size}
-        Learning rate:   {learning_rate}
+        Epochs:          {args.epochs}
+        Batch size:      {args.batch_size}
+        Learning rate:   {args.lr}
         Training size:   {n_train}
         Validation size: {n_val}
         Checkpoints:     {save_checkpoint}
         Device:          {device.type}
-        Images scaling:  {img_scale}
-        Mixed Precision: {amp}
+        Images scaling:  {args.img_scale}
+        Mixed Precision: {args.amp}
     ''')
 
     # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss scaling for AMP
-    optimizer = optim.RMSprop(net.parameters(), lr=learning_rate, weight_decay=1e-8, momentum=0.9)
+    optimizer = optim.RMSprop(net.parameters(), lr=args.lr, weight_decay=1e-8, momentum=0.9)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=2)  # goal: maximize Dice score
-    grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
+    grad_scaler = torch.cuda.amp.GradScaler(enabled=args.amp)
     criterion = nn.CrossEntropyLoss()
     global_step = 0
 
     # 5. Begin training
-    for epoch in range(1, epochs+1):
+    for epoch in range(1, args.epochs+1):
         net.train()
         epoch_loss = 0
-        with tqdm(total=n_train, desc=f'Epoch {epoch}/{epochs}', unit='img') as pbar:
+        with tqdm(total=n_train, desc=f'Epoch {epoch}/{args.epochs}', unit='img') as pbar:
             for batch in train_loader:
                 images = batch['image']
                 true_masks = batch['mask']
@@ -87,7 +81,7 @@ def train_net(net,
                 images = images.to(device=device, dtype=torch.float32)
                 true_masks = true_masks.to(device=device, dtype=torch.long) # for one hot encoding
 
-                with torch.cuda.amp.autocast(enabled=amp):
+                with torch.cuda.amp.autocast(enabled=args.amp):
                     masks_pred = net(images)
                     loss = criterion(masks_pred, true_masks) \
                            + dice_loss(F.softmax(masks_pred, dim=1).float(),
@@ -110,7 +104,7 @@ def train_net(net,
                 pbar.set_postfix(**{'loss (batch)': loss.item()})
 
                 # Evaluation round
-                division_step = (n_train // (10 * batch_size))
+                division_step = (n_train // (10 * args.batch_size))
                 if division_step > 0:
                     if global_step % division_step == 0:
                         # histograms = {}
@@ -141,26 +135,26 @@ def train_net(net,
             torch.save(net.state_dict(), str(dir_checkpoint / f'checkpoint-{experiment.name}_epoch-{epoch}.pth'))
             logging.info(f'Checkpoint {epoch} saved!')
 
-
-def get_args():
-    parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
-    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
-    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
-    parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=1e-5,
-                        help='Learning rate', dest='lr')
-    parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
-    parser.add_argument('--scale', '-s', type=float, default=0.5, help='Downscaling factor of the images')
-    parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
-                        help='Percent of the data that is used as validation (0-100)')
-    parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
-    parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
-    parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
-
-    return parser.parse_args()
-
-
 if __name__ == '__main__':
-    args = get_args()
+    # Default parameters
+    hyperparameter_defaults = dict(
+        epochs=5, 
+        dir_img='tc/img/', # defaults to data/ + data_path
+        dir_mask='tc/maskT', # same as above
+        batch_size = 10,
+        lr = 1e-3, # will lower during training
+        load=False, # Load model from a .pth file
+        img_scale=0.5, # Downscaling factor of the images
+        val=10.0, # Percent of the data that is used as validation (0-100)
+        amp=False, # Use mixed precision
+        bilinear = False, # Use bilinear upsampling
+        n_classes=2, # Number of classes
+        n_channels=3, # 3 for RGB inputs
+    )
+
+    experiment = wandb.init(config=hyperparameter_defaults)
+    # Config parameters are automatically set by W&B sweep agent
+    args = wandb.config
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
@@ -169,7 +163,7 @@ if __name__ == '__main__':
     # Change here to adapt to your data
     # n_channels=3 for RGB images
     # n_classes is the number of probabilities you want to get per pixel
-    net = UNet(n_channels=3, n_classes=2, bilinear=args.bilinear)
+    net = UNet(n_channels=args.n_channels, n_classes=args.n_classes, bilinear=args.bilinear)
 
     logging.info(f'Network:\n'
                  f'\t{net.n_channels} input channels\n'
@@ -182,14 +176,7 @@ if __name__ == '__main__':
 
     net.to(device=device)
     try:
-        train_net(net=net,
-                  epochs=args.epochs,
-                  batch_size=args.batch_size,
-                  learning_rate=args.lr,
-                  device=device,
-                  img_scale=args.scale,
-                  val_percent=args.val / 100,
-                  amp=args.amp)
+        train_net(net, args, experiment, save_checkpoint=True)
     except KeyboardInterrupt:
         torch.save(net.state_dict(), 'INTERRUPTED.pth')
         logging.info('Saved interrupt')
