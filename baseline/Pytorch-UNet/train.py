42bb1da7c4166fe370a271ce816d7c72674ada42
Garth Wales <walga757@student.otago.ac.nz>
2022-09-20 23:56:00 +1200

    switch back to sgd

--- baseline/Pytorch-UNet/train.py
+++ baseline/Pytorch-UNet/train.py
@@ -181,7 +181,7 @@ if __name__ == '__main__':
         img_size = (16,16),
 
         # NOT USED YET
-        optim='adam',
+        optim='sgd',
         shuffle=True,
 
         # whether to have a network at the end
