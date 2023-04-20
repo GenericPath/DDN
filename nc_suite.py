4e1524573b5c65157e94d86a865c73d506627680
Garth Wales <git@garth.wales>
2023-04-20 15:02:40 +1200

    better plotting

--- nc_suite.py
+++ nc_suite.py
@@ -98,7 +98,51 @@ def weights_2(img):
 
     return W
 
-def plot_images(imgs, labels=None, colmns=None):
+def add_headers(
+    fig,
+    *,
+    row_headers=None,
+    col_headers=None,
+    row_pad=1,
+    col_pad=5,
+    rotate_row_headers=True,
+    **text_kwargs
+):
+    # Based on https://stackoverflow.com/a/25814386
+
+    axes = fig.get_axes()
+
+    for ax in axes:
+        sbs = ax.get_subplotspec()
+
+        # Putting headers on cols
+        if (col_headers is not None) and sbs.is_first_row():
+            ax.annotate(
+                col_headers[sbs.colspan.start],
+                xy=(0.5, 1),
+                xytext=(0, col_pad),
+                xycoords="axes fraction",
+                textcoords="offset points",
+                ha="center",
+                va="baseline",
+                **text_kwargs,
+            )
+
+        # Putting headers on rows
+        if (row_headers is not None) and sbs.is_first_col():
+            ax.annotate(
+                row_headers[sbs.rowspan.start],
+                xy=(0, 0.5),
+                xytext=(-ax.yaxis.labelpad - row_pad, 0),
+                xycoords=ax.yaxis.label,
+                textcoords="offset points",
+                ha="right",
+                va="center",
+                rotation=rotate_row_headers * 90,
+                **text_kwargs,
+            )
+
+def plot_images(imgs, labels=None, row_headers=None, col_headers=None, colmns=None):
     """
     imgs = [img1, img2]
     labels = ['label1', 'label2']
@@ -122,6 +166,8 @@ def plot_images(imgs, labels=None, colmns=None):
             sub.set_title(f'{labels[i-1]}')
         sub.axis('off')
         sub.imshow(imgs[i-1])
+        
+    add_headers(fig, row_headers, col_headers)
     plt.tight_layout()
 
 def DW_matrices(graph):
